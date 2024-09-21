06f9698255b34180468506d9d2ec4a326d9d4687
nathanaelsanilo <nathanaelsanilo@gmail.com>
2024-09-21 12:55:31 +0700

    feat: add google maps service

--- MyShoppingList/app/build.gradle.kts
+++ MyShoppingList/app/build.gradle.kts
@@ -51,6 +51,21 @@ android {
 
 dependencies {
 
+    // google maps
+    implementation ("com.google.maps.android:maps-compose:2.15.0")
+    implementation ("com.google.android.gms:play-services-maps:18.1.0")
+    implementation("com.google.android.gms:play-services-location:21.0.1")
+
+    // view model
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
+
+    // retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
+    // navigation
+    implementation("androidx.navigation:navigation-compose:2.7.4")
+
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.4")
     implementation("androidx.activity:activity-compose:1.9.1")
--- MyShoppingList/app/src/main/java/com/example/myshoppinglist/ShoppingList.kt
+++ MyShoppingList/app/src/main/java/com/example/myshoppinglist/ShoppingList.kt
@@ -1,5 +1,10 @@
 package com.example.myshoppinglist
 
+import android.Manifest
+import android.content.Context
+import android.widget.Toast
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
@@ -17,6 +22,7 @@ import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.LocationOn
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
@@ -32,22 +38,64 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import androidx.core.app.ActivityCompat
+import androidx.navigation.NavController
 
 data class ShoppingItem(
-    val id: Int, var name: String, var quantity: Int, var isEditing: Boolean = false
+    val id: Int,
+    var name: String,
+    var quantity: Int,
+    var isEditing: Boolean = false,
+    var address: String = ""
 )
 
 @Composable
-fun ShoppingListApp() {
+fun ShoppingListApp(
+    locationUtils: LocationUtils,
+    viewModel: LocationViewModel,
+    navController: NavController,
+    context: Context,
+    address: String
+) {
     var sItems by remember { mutableStateOf(listOf<ShoppingItem>()) }
     var showDialog by remember { mutableStateOf(false) }
     var itemName by remember { mutableStateOf("") }
     var itemQuantity by remember { mutableStateOf("") }
 
+    val requestPermissionLauncher =
+        rememberLauncherForActivityResult(contract = ActivityResultContracts.RequestMultiplePermissions(),
+            onResult = { permissions ->
+                if (permissions[Manifest.permission.ACCESS_COARSE_LOCATION] == true && permissions[Manifest.permission.ACCESS_FINE_LOCATION] == true) {
+                    locationUtils.requestLocationUpdates(viewModel = viewModel)
+                } else {
+                    val rationaleRequired = ActivityCompat.shouldShowRequestPermissionRationale(
+                        context as MainActivity, Manifest.permission.ACCESS_FINE_LOCATION
+                    ) || ActivityCompat.shouldShowRequestPermissionRationale(
+                        context as MainActivity, Manifest.permission.ACCESS_COARSE_LOCATION
+                    )
+
+                    if (rationaleRequired) {
+                        Toast.makeText(
+                            context,
+                            "Location Permission is required for this feature to work",
+                            Toast.LENGTH_LONG
+                        ).show()
+                    } else {
+                        Toast.makeText(
+                            context,
+                            "Location Permission is required. Please enable it in the Android Settings",
+                            Toast.LENGTH_LONG
+                        ).show()
+                    }
+                }
+            })
+
+
     Column(
         modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center
     ) {
-        Button(modifier = Modifier.align(Alignment.CenterHorizontally),
+        Button(
+            modifier = Modifier.align(Alignment.CenterHorizontally),
             onClick = { showDialog = true }) {
             Text(text = "Add item")
         }
@@ -64,17 +112,15 @@ fun ShoppingListApp() {
                         currentItem?.let {
                             it.name = editedName
                             it.quantity = editedQuantity
+                            it.address = address
                         }
                     })
                 } else {
-                    ShoppingListItem(
-                        item,
-                        onEdit = {
-                            sItems = sItems.map { it.copy(isEditing = item.id == it.id) }
-                        },
-                        onDelete = {
-                            sItems = sItems - item
-                        })
+                    ShoppingListItem(item, onEdit = {
+                        sItems = sItems.map { it.copy(isEditing = item.id == it.id) }
+                    }, onDelete = {
+                        sItems = sItems - item
+                    })
                 }
 
             }
@@ -92,9 +138,7 @@ fun ShoppingListApp() {
                 Button(onClick = {
                     if (itemName.isNotBlank()) {
                         val newItem = ShoppingItem(
-                            id = sItems.size + 1,
-                            name = itemName,
-                            quantity = itemQuantity.toInt()
+                            id = sItems.size + 1, name = itemName, quantity = itemQuantity.toInt()
                         )
 
                         sItems += newItem
@@ -126,6 +170,24 @@ fun ShoppingListApp() {
                         .fillMaxWidth()
                         .padding(6.dp)
                 )
+
+                Button(onClick = {
+                    if (locationUtils.hasLocationPermission(context)) {
+                        locationUtils.requestLocationUpdates(viewModel)
+                        navController.navigate("location-screen") {
+                            this.launchSingleTop
+                        }
+                    } else {
+                        requestPermissionLauncher.launch(
+                            arrayOf(
+                                Manifest.permission.ACCESS_FINE_LOCATION,
+                                Manifest.permission.ACCESS_COARSE_LOCATION
+                            )
+                        )
+                    }
+                }) {
+                    Text("address")
+                }
             }
         })
     }
@@ -142,8 +204,23 @@ fun ShoppingListItem(
             .fillMaxWidth()
             .border(border = BorderStroke(2.dp, Color.Cyan), shape = RoundedCornerShape(20))
     ) {
-        Text(text = item.name, modifier = Modifier.padding(8.dp))
-        Text(text = "Quantity: ${item.quantity}", modifier = Modifier.padding(8.dp))
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .padding(8.dp)
+        ) {
+            Row() {
+                Text(text = item.name, modifier = Modifier.padding(8.dp))
+                Text(text = "Quantity: ${item.quantity}", modifier = Modifier.padding(8.dp))
+            }
+
+            Row(modifier = Modifier.fillMaxWidth()) {
+                Icon(imageVector = Icons.Default.LocationOn, contentDescription = null)
+                Text(text = item.address)
+            }
+
+        }
+
 
         Row(modifier = Modifier.padding(8.dp)) {
             IconButton(onClick = onEdit) {
@@ -166,18 +243,25 @@ fun ShoppingItemEditor(item: ShoppingItem, onComplete: (String, Int) -> Unit) {
         modifier = Modifier
             .padding(8.dp)
             .fillMaxWidth()
-            .background(Color.White), horizontalArrangement = Arrangement.SpaceEvenly
+            .background(Color.White),
+        horizontalArrangement = Arrangement.SpaceEvenly
     ) {
         Column {
             BasicTextField(
-                value = name, onValueChange = { name = it }, modifier = Modifier
+                value = name,
+                onValueChange = { name = it },
+                modifier = Modifier
                     .wrapContentSize()
-                    .padding(8.dp), singleLine = true
+                    .padding(8.dp),
+                singleLine = true
             )
             BasicTextField(
-                value = quantity, onValueChange = { quantity = it }, modifier = Modifier
+                value = quantity,
+                onValueChange = { quantity = it },
+                modifier = Modifier
                     .wrapContentSize()
-                    .padding(8.dp), singleLine = true
+                    .padding(8.dp),
+                singleLine = true
             )
         }
 
